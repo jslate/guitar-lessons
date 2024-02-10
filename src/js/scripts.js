7b20a52afcb45b03e06dbf93fa2072d53c430350
Jonathan Slate <jonathan.slate@pmglearning.com>
2024-02-09 21:22:04 -0500

    more pages and qr

---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+new QRCode(document.getElementById("qrcode"), {
+  text: window.location.href,
+  width: 128,
+  height: 128,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
